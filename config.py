7a99c5e23fb06c56adfc1debb264667f0757c063
B4sco <107991533+B4sco@users.noreply.github.com>
2024-03-19 16:20:28 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SCREEN_WIDTH = 3840
+    SCREEN_HEIGHT = 2160
+    SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+    desired_fps = 1
+    date1 = '2024-03-05 12:00:00'
+    date2 = '2024-03-05 12:01:00'
