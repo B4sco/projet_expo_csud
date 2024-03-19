5583632e814a1f6909954e2b7a2be96e3c62fcc5
B4sco <107991533+B4sco@users.noreply.github.com>
2024-03-19 16:21:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,11 @@ import sys
 import os
 import random
 import time
+from config import Config
  
 from pygame.locals import *
 
 from datetime import datetime
-
- 
-class Config:
-    SCREEN_WIDTH = 3840
-    SCREEN_HEIGHT = 2160
-    SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-    desired_fps = 1
-    date1 = '2024-03-05 12:00:00'
-    date2 = '2024-03-05 12:01:00'
     
 def quit():
     pygame.quit()
